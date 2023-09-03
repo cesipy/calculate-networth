0e717d99d55b89523c4ed73e271893ef4c2d0373
cedric <cedric.sillaber@gmail.com>
2023-09-03 11:57:47 +0200

    refactor

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import time
 import matplotlib.pyplot as plt
 import datetime
 
+# load current eur-usd exchange rate
+tickerSymbol = 'EURUSD=X'
+tickerData = yf.Ticker(tickerSymbol)
+exchangeRate = tickerData.info['regularMarketOpen']
+
 # assets in eur
 EUROS = ['VWCE.DE', 'M44.BE', 'EUNM.DE', '82W.BE']
 
@@ -46,43 +51,60 @@ def read_from_csv():
     return (dates_array, values_array)
 
 
-def main():
-    # load current eur-usd exchange rate
-    tickerSymbol = 'EURUSD=X'
-    tickerData = yf.Ticker(tickerSymbol)
-    exchangeRate = tickerData.info['regularMarketOpen']
+def read_positions():
+    tickers = []
+    amounts = []
+    with open("positions.csv", mode="r") as f:
+        reader = csv.reader(f)
 
-    # load positions from file
-    with open("positions.txt") as f:
-        positions = [line.strip().split(",") for line in f]
+        # skip header
+        header = next(reader, None)
+
+        for row in reader:
+            ticker = row[0]
+            amount = float(row[1])
+
+            # add to arrays
+            tickers.append(ticker)
+            amounts.append(amount)
+
+    return tickers, amounts
+
+def main():
+    # load positions from .csv
+    tickers, amounts = read_positions()
 
     # calculate total value of all positions
     total_value = 0
-    for i in positions:
+    for i in range(len(tickers)):
         value = 0
-        nested_arr = i[0].split()
-        ticker = nested_arr[0]
-        ticker = ticker[1:-1]
-        num_shares = float(nested_arr[1])
+
+        ticker = tickers[i]
+
+        num_shares = amounts[i]
 
         value = yf.Ticker(ticker).info['regularMarketOpen'] * num_shares
-        # special handling:
+        # special handling for euro assets:
         if ticker in EUROS:
-            # print("reached!")
+            # calculate usd value of euro assets
             value = value * exchangeRate
 
+        # print to terminal
         formatted_output = "ticker: {:<{width}} worth: {:{width}.2f}".format(ticker, value, width=15)
         print(formatted_output)
 
         total_value += value
 
+    # convert to euro
     total_value /= exchangeRate
     total_value = round(total_value, 2)
 
+    # print total value of portfolio
     print(f"Total portfolio value in €: {total_value:.2f}")
 
     # add to csv
     save_to_file(total_value)
 
 
-plot_networth()
+if __name__ == '__main__':
+    main()
