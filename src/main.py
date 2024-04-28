79b795ece73a52a93b81db3687bef72262896eb7
Cedric Sillaber <cedric.sillaber@gmail.com>
2024-04-29 00:17:42 +0200

    add also initial investment value

--- src/main.py
+++ src/main.py
@@ -28,12 +28,12 @@ def calculate_value(ticker: str, num_shares: float) -> float:
     return yf.Ticker(ticker).info['regularMarketOpen'] * num_shares
 
 
-def write_networth_to_file(value: float):
+def write_networth_to_file(current_value: float, invested_money: float ):
     """
     saves current networth `value`.
     """
     current_time = int(time.time())
-    new_data     = {'time': str(current_time), 'value': str(value)}
+    new_data     = {'time': str(current_time), 'value': str(current_value), 'investment_value':str(invested_money)}
 
        # Read existing data
     try:
@@ -52,13 +52,15 @@ def plot_networth_history():
     """
     plots networth history in a coordinate system using time in x axis and value on y axis.
     """
-    dates, values = load_networth_history()
+    dates, values, investment_values = load_networth_history()
 
     plt.figure(figsize=(10, 6))
     
-    plt.plot(dates, values)
+    plt.plot(dates, values, label='Net Worth')
+    plt.plot(dates, investment_values, color='green', label='Investment Value')
     plt.xlabel("Date")
-    plt.ylabel("Networth in €")
+    plt.ylabel("Value in €")
+    plt.legend()
 
     plt.show()
 
@@ -69,6 +71,7 @@ def load_networth_history() -> typing.Tuple[typing.List[int], typing.List[int]]:
     """
     values_array = []
     dates_array = []
+    investment_values = []
     with open(NETWORTH_FILE_PATH, mode="r") as f:
         data_list = json.load(f)
     
@@ -76,11 +79,13 @@ def load_networth_history() -> typing.Tuple[typing.List[int], typing.List[int]]:
         unix_timestamp = int(data['time'])
         formatted_date = datetime.datetime.fromtimestamp(unix_timestamp).date()
         value          = float(data['value'])
+        investment_value = float(data['investment_value'])
 
         values_array.append(value)
         dates_array.append(formatted_date)
+        investment_values.append(investment_value)
 
-    return (dates_array, values_array)
+    return (dates_array, values_array, investment_values)
 
 
 def load_positions() -> typing.Tuple[typing.List[str], typing.List[int]]:
@@ -89,6 +94,7 @@ def load_positions() -> typing.Tuple[typing.List[str], typing.List[int]]:
     """
     tickers = []
     amounts = []
+    avg_prices = []
     
     with open(POSITIONS_FILE_PATH, "r") as f: 
         data_list = json.load(f)
@@ -96,9 +102,9 @@ def load_positions() -> typing.Tuple[typing.List[str], typing.List[int]]:
     for data in data_list:
         tickers.append(data['ticker'])
         amounts.append(float(data['amount']))
+        avg_prices.append(float(data['avg_buy_price']))
 
-
-    return tickers, amounts
+    return tickers, amounts, avg_prices
 
 
 def handle_cli() -> bool:
@@ -128,39 +134,52 @@ def main():
     flag = handle_cli()
 
     # load positions from .csv
-    tickers, amounts = load_positions()
+    tickers, amounts, avg_prices = load_positions()
 
     # calculate total value of all positions
-    total_value = 0
+    total_current_value = 0
+    total_buy_value     = 0
     for i in range(len(tickers)):
         value = 0
+        buy_amount = 0
 
         ticker = tickers[i]
-
         num_shares = amounts[i]
+        avg_price  = avg_prices[i]
 
-        #value = yf.Ticker(ticker).info['regularMarketOpen'] * num_shares+
         value = calculate_value(ticker, num_shares)
+
+        if ticker.startswith("Bank"):
+            buy_amount = num_shares
+        else: 
+            buy_amount = num_shares * avg_price
+
         # special handling for euro assets:
         if ticker in EUROS:
             # calculate usd value of euro assets
             value = value * exchangeRate
+            buy_amount = buy_amount * exchangeRate
 
         formatted_output = "ticker: {:<{width}} worth: {:{width}.2f}".format(ticker, value, width=15)
         logger.log(formatted_output)
         print(formatted_output)
 
-        total_value += value
+        total_current_value += value
+        total_buy_value += buy_amount
 
     # convert to euro
-    total_value /= exchangeRate
-    total_value = round(total_value, 2)
+    total_current_value /= exchangeRate
+    total_current_value = round(total_current_value, 2)
 
-    logger.log(f"Total portfolio value in €: {total_value:.2f}")
-    print(f"Total portfolio value in €: {total_value:.2f}")
+    total_buy_value /= exchangeRate
+    total_buy_value = round(total_buy_value, 2)
+
+    logger.log(f"Total portfolio value in €: {total_current_value:.2f}")
+    print(f"Total portfolio value in €: {total_current_value:.2f}")
+    print(f"Invested value in €: {total_buy_value:.2f}")
 
     # add to csv
-    write_networth_to_file(total_value)
+    write_networth_to_file(total_current_value, invested_money=total_buy_value)
 
     if flag:
         plot_networth_history()
